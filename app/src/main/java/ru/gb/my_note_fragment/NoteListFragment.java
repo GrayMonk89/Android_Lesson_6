8cf36c02f26cb06d664f5b6b2ab8bb36a24952a4
Gray Monk <graymonk89@yandex.ru>
2022-02-26 15:21:17 +0300

    8 урок. Добавил popup для элементов списка заметок

--- app/src/main/java/ru/gb/my_note_fragment/NoteListFragment.java
+++ app/src/main/java/ru/gb/my_note_fragment/NoteListFragment.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -66,6 +69,35 @@ public class NoteListFragment extends Fragment {
                     } else {
                         showPort();
                     }
+                }
+            });
+            textView.setOnLongClickListener(new View.OnLongClickListener() {
+
+
+
+                @Override
+                public boolean onLongClick(View view) {
+                    PopupMenu popupMenu = new PopupMenu(requireContext(), view);
+                    requireActivity().getMenuInflater().inflate(R.menu.note_list_popup, popupMenu.getMenu());
+                    popupMenu.show();
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()){
+                                case (R.id.action_edit_list_item):{
+                                    Toast.makeText(requireContext(), "Типо редактируем", Toast.LENGTH_LONG).show();
+                                    return true;
+                                }
+                                case (R.id.action_delete_list_item):{
+                                    Toast.makeText(requireContext(), "Типо удаляем", Toast.LENGTH_LONG).show();
+                                    return true;
+                                }
+                            }
+                            return true;
+                        }
+                    });
+
+                    return true;
                 }
             });
         }
